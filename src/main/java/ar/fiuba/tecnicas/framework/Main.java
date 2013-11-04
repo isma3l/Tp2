0e8d14fe28697e3739b24aaec3e6ee632e8ea3ca
luis <aliluis@gmail.com>
2013-11-04 17:33:34 -0300

    El cliente del composite en realidad es main. TestWithFramework no tenia sentido

--- src/main/java/ar/fiuba/tecnicas/framework/Main.java
+++ src/main/java/ar/fiuba/tecnicas/framework/Main.java
@@ -1,16 +1,12 @@
 package ar.fiuba.tecnicas.framework;
 
-import static org.hamcrest.CoreMatchers.is;
-
 public class Main {
-
-
     public static void main(String[] args) {
         AssertsBuilder assertbuilder= AssertsBuilder.getInstance();
         Test tests= assertbuilder.makeAll();
         Test assertTest=assertbuilder.makePart();
         tests.addTest(assertTest);
-        TestWithFramework testSuite = new TestWithFramework(tests);
-        testSuite.excecute();
+        TestReport testReport= new TestReport();
+        tests.excecute(testReport);
     }
 }
